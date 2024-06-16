0354a2b1120606f8ccf7bc1b9dd470b84f22eb59
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-16 21:23:06 +0300

    add

--- main.cpp
+++ main.cpp
@@ -2,9 +2,37 @@
 #include <Windows.h>
 using namespace std;
 
+bool is_perfect(int n) {
+    if (n <= 1) return false;
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+    return sum == n;
+}
+
+void find_perfect_numbers(int start, int end) {
+    for (int i = start; i <= end; i++) {
+        if (is_perfect(i)) {
+            cout << i << " - досконале число" << endl;
+
+        }
+    }
+}
+    
+
 int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+    int start, end;
+    cout << "¬вед≥ть д≥апазон (початкове та к≥нцеве значенн€): ";
+    cin >> start >> end;
+
+    if (start > end) {
+        swap(start, end);
+    }
+
+    find_perfect_numbers(start, end);
 
     return 0;
 }
