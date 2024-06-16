9349fca1dcd28f4b8cf602d9d1e9a2bd3a5d1427
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-16 17:02:53 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 #include <Windows.h>
 using namespace std;
 
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+void factorial() {
 
-    return 0;
+	int factorial = 1, number;
+	cout << "Введіть число для визначення факторіалу: ";
+	cin >> number;
+	for (int i = 1; i <= number; i++)
+	{
+		factorial *= i;
+	}
+	cout << endl;
+	cout << "Факторіал " << number << " є " << factorial << endl;
+
 }
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	factorial();
+
+
+	return 0;
+}
+
