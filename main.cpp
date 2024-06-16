2b810c28db570fcc722190b8747f2b876e1ca49f
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-16 17:02:08 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -2,9 +2,44 @@
 #include <Windows.h>
 using namespace std;
 
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+void prum() {
 
-    return 0;
+	int height, width;
+	cout << "¬вед≥ть висоту: ";
+	cin >> height;
+	cout << "¬вед≥ть ширину: ";
+	cin >> width;
+	for (int i = 0; i < width; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 0; i < height - 2; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0 || j == width - 1) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+
+		cout << endl;
+	}
+	for (int i = 0; i < width; i++)
+	{
+		cout << "*";
+	}
+
 }
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	prum();
+
+
+	return 0;
+}
+
