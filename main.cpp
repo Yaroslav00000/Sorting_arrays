46c60016f7019ddaee4945d0cc28a3052dddc159
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-16 17:13:49 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -2,9 +2,27 @@
 #include <Windows.h>
 using namespace std;
 
+int Prime_number() {
+    int num;
+    cout << "Введіть число: ";
+    cin >> num;
+    if (num < 2) {
+		cout << "Число не є простим" << endl;
+		return false;
+	}
+    for (int i = 2; i <= num / 2; i++) {
+		if (num % i == 0) {
+			cout << "Число не є простим" << endl;
+			return false;
+		}
+	}
+    cout << "Число є простим" << endl;
+}
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    Prime_number();
 
     return 0;
 }
