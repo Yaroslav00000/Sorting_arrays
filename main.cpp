603a992aea9cddc754239e0cc99210d256499ef4
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-16 22:03:14 +0300

    add

--- main.cpp
+++ main.cpp
@@ -2,9 +2,30 @@
 #include <Windows.h>
 using namespace std;
 
+void Happy_number(int n) {
+	int sum1 = 0;
+    int sum2 = 0;
+	n = 123321;
+    for (int i = 0; i <= 2; i++) {
+		sum1 += i;
+	}
+    for (int i = 5; i <= 3; i--) {
+		sum2 += i;
+	}
+	if (sum1 == sum2) {
+		cout << "Happy number" << endl;
+	}
+	else {
+		cout << "Not a happy number" << endl;
+	}
+    
+}
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    Happy_number(123321);
     return 0;
 }
