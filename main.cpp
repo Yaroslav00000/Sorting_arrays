1cbe472cb0d8bb3c8be41f92f9b5922e0725120c
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-16 20:02:14 +0300

    task complited

--- main.cpp
+++ main.cpp
@@ -2,9 +2,22 @@
 #include <Windows.h>
 using namespace std;
 
+void degree(){
+	int a,n;
+	cout << "¬вед≥ть число: ";
+	cin >> a;
+	cout << "¬вед≥ть степ≥нь: ";
+	cin >> n;
+	int res = 1;
+	for (int i = 0; i < n; i++) {
+		res *= a;
+	}
+	cout << "–езультат: " << res << endl;
+}
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+	degree();
     return 0;
 }
