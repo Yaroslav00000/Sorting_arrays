7d18c77694366eb89ab647014e10c9368dfd7b06
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-16 19:35:59 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -2,9 +2,22 @@
 #include <Windows.h>
 using namespace std;
 
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+void kyb() {
 
-    return 0;
+	int kyb_number = 0, number;
+	cout << "Введіть число для визначення його кубу: ";
+	cin >> number;
+	kyb_number = number * number;
+	cout << endl;
+	cout << "Куб " << number << " є " << kyb_number << endl;
+
 }
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	kyb();
+
+
+	return 0;
+}
+
